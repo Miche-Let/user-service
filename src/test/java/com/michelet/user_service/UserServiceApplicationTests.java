1190b342b082e861b7d9db1925ea4cb6c6737153
Sehi55 <sehi1028@naver.com>
2026-04-25 03:19:34 +0900

    feat: test eureka setting

--- src/test/java/com/michelet/user_service/UserServiceApplicationTests.java
+++ src/test/java/com/michelet/user_service/UserServiceApplicationTests.java
@@ -2,7 +2,9 @@ package com.michelet.user_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class UserServiceApplicationTests {
 
