139ad54821e7cb4b34229f4607b14df26dc9761c
임세희 <109964874+Sehi55@users.noreply.github.com>
2026-04-26 10:12:43 +0900

    chore: 프로젝트 초기 설정 및 hello world api 추가 (#2)
    * chore: 프로젝트 초기 설정 및 hello world api 추가
    
    * chore: add gitignore file
    
    * fix: edit HELP.md
    
    * fix: edit project group
    
    * fix:edit  project group name
    
    * fix: edit test package name
    
    * feat: test eureka setting
    
    * fix: edit project name
    
    * chore: add .DS_Store in .gitignore and change gradle version
    
    * chore: unstage .DS_Store
    
    * chore: add application.yml
    
    * fix: edit eureka port number

---
+++ src/main/java/com/michelet/user/HelloController.java
@@ -0,0 +1,13 @@
+package com.michelet.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/users/hello")
+    public String hello() {
+        return "hello from user-service";
+    }
+}
