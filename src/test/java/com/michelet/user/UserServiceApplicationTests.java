aeb430f4ebd18985b256c0d93f742ccecf59f005
Sehi55 <sehi1028@naver.com>
2026-04-29 22:40:11 +0900

    test: import redisconfig.class

--- src/test/java/com/michelet/user/UserServiceApplicationTests.java
+++ src/test/java/com/michelet/user/UserServiceApplicationTests.java
@@ -1,11 +1,14 @@
 package com.michelet.user;
 
+import com.michelet.user.config.RedisConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@Import(RedisConfig.class)
 class UserServiceApplicationTests {
 
 	@Test
